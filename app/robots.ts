e1d29cc2df4a0bb6dfc32dc32872322ec4159607
Ela Teknoloji <hytasarim@gmail.com>
2026-07-25 17:37:33 +0300

    merchant hat düzektme

--- app/robots.ts
+++ app/robots.ts
@@ -2,20 +2,37 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/wp-content/', 
-        '/wp-includes/', 
-        '/sepet/', 
-        '/odeme/', 
-        '/my-account/', 
-        '/*?attribute_pa_*', 
-        '/*/pdf',
-        '/*?gridcookie=*'
-      ],
-    },
+    rules: [
+      // 1. Merchant Center ve Arama Motoru Botu İçin Tam İzin
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/sepet/', 
+          '/odeme/', 
+          '/my-account/',
+          '/*/pdf'
+        ],
+      },
+      // 2. Görsel Botu İçin Tam İzin (Ürün resimlerinin Merchant Center'a gitmesi için şart)
+      {
+        userAgent: 'Googlebot-image',
+        allow: '/',
+      },
+      // 3. Diğer Tüm Botlar İçin Genel Kurallar (wp-content engeli kaldırıldı)
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/sepet/', 
+          '/odeme/', 
+          '/my-account/', 
+          '/*?attribute_pa_*', 
+          '/*?gridcookie=*',
+          '/*/pdf'
+        ],
+      }
+    ],
     sitemap: 'https://www.veluxe.com.tr/sitemap.xml',
   };
 }
